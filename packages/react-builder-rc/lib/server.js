3d801aba0e4d9e49f93c800c700be527e435e417
mengbo-ji <jmb33595@163.com>
2021-09-07 11:07:36 +0800

    feat: 端口占用另开

--- packages/react-builder-rc/lib/server.js
+++ packages/react-builder-rc/lib/server.js
@@ -2,25 +2,11 @@
 const WebpackDevServer = require('webpack-dev-server');
 const portfinder = require('portfinder');
 const chalk = require('chalk');
-const { LOG_COLOR } = require('./constant');
-
-function getPort(devServer) {
-  return new Promise((resolve, reject) => {
-    portfinder.getPort((err, port) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      console.log(chalk.cyan('INFO:'), `Port ${chalk.hex(LOG_COLOR)(devServer.port)} is in use, trying another one...`);
-      devServer.port = port;
-      resolve(devServer);
-    });
-  });
-}
 
 module.exports = async function(devServer, compiler) {
-  const { port: PORT, host: HOST } = await getPort(devServer);
+  portfinder.basePort = devServer.port || 3000;
+  const PORT = await portfinder.getPortPromise();
+  const HOST = devServer.host;
   const httpServer = new WebpackDevServer(compiler, devServer);
 
   httpServer.listen(PORT, HOST, err => {
